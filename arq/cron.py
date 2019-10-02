84f51f38f9a2f386b9e3581230ee9c41de961a1a
Ward Fenton <ward.fenton@oath.com>
2019-10-02 16:46:11 -0400

    refactoring mixup

--- arq/cron.py
+++ arq/cron.py
@@ -169,7 +169,7 @@ def cron(
         name = name or 'cron:' + coroutine
         coroutine = import_string(coroutine)
 
-    assert asyncio.iscoroutine_or_partial(coroutine), f'{coroutine} is not a coroutine or partial function'
+    assert iscoroutine_or_partial(coroutine), f'{coroutine} is not a coroutine function or a partial'
     timeout = to_seconds(timeout)
     keep_result = to_seconds(keep_result)
 
